9ca4129f2c209762dd7b24b8320028ee528920f8
cth <172237270@qq.com>
2020-05-12 09:27:48 +0800

    gengxinzhong

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    type: [],
+      xilie: [],
+      zhuda: [],
+      like: [],
+      list:[]
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    
+  }
+})
